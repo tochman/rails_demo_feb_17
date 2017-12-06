2ee68997cfb83aa01538fc6c5e98fa25539b3d01
tochman <thomas@craftacademy.se>
2017-12-06 09:54:16 +0100

    adds current_user.email to stripe form

--- features/step_definitions/stripe_steps.rb
+++ features/step_definitions/stripe_steps.rb
@@ -6,7 +6,6 @@ And(/^I fill in my card details on the stripe form$/) do
   sleep(2)
   @stripe_iframe = all('iframe[name=stripe_checkout_app]').last
   within_frame @stripe_iframe do
-    fill_in 'Email', with: 'random@morerandom.com'
     fill_in 'Card number', with: '4242 4242 4242 4242'
     fill_in 'CVC', with: '123'
     fill_in 'Expiry', with: '12/2021'
