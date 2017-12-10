c954a091c7c6c7d9b64cb063011d44d721d3a87b
tochman <thomas@craftacademy.se>
2017-12-10 11:16:17 +0100

    refactors facebook oauth strategy - 2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,9 +251,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook,
-                  '1621623384764301',
-                  '52edb8b7ca0af425a4cb406781004803', {scope: 'public_profile'}
+
+  config.omniauth :facebook, ENV['FACEBOOK_OAUTH_KEY'], ENV['FACEBOOK_OAUTH_SECRET'], {scope: 'public_profile', callbackURL: '/auth/facebook/callback/'}
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
